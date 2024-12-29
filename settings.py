d446d090fd511cbf71958fe5be77d4a6d439f116
andrew.knowles <andrewkapranos@gmail.com>
2024-12-29 13:57:55 +1000

    End of Chapter 1 - working openGL Window

--- settings.py
+++ settings.py
@@ -1,7 +1,10 @@
 from numba import njit
 import numpy as np
-import gml
 import math
+import glm
 
 # Game Resolution
-WIN_RES = gml.vec2(1600, 900)
+WIN_RES = glm.vec2(1600,900)
+
+#Colours
+BG_COLOUR = glm.vec3(0.1,0.16,0.25)
