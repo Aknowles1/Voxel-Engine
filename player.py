39702ea1f8eb0dd3f84c775ab5768e99f85e178c
andrew.knowles <andrewkapranos@gmail.com>
2024-12-31 21:52:25 +1000

    Gravity fixed

--- player.py
+++ player.py
@@ -3,20 +3,23 @@ import glm
 from camera import Camera
 from settings import *
 
-GRAVITY = 2
+GRAVITY = 0.5
 PLAYER_HALF_WIDTH = 0.3
-PLAYER_HEIGHT = 3.5
+PLAYER_HEIGHT = 1.8
 HALF_HEIGHT = PLAYER_HEIGHT * 0.5
-JUMP_VELOCITY = 1.5
+JUMP_VELOCITY = 0.2
+
+# How high you can step up (in blocks). 1.0 => can step 1 block.
+STEP_OFFSET = 1.0  
 
 class Player(Camera):
     def __init__(self, app, position=PLAYER_POS, yaw=-90, pitch=0):
         """
         We'll treat 'position' as the center of our bounding box.
-        You can place the camera's 'eye' above center if you'd like.
         """
         self.app = app
         super().__init__(position, yaw, pitch)
+
         self.velocity = glm.vec3(0, 0, 0)
         self.on_ground = False
 
@@ -27,6 +30,9 @@ class Player(Camera):
         super().update()
 
     def apply_gravity(self):
+        """
+        Applies gravity using partial-snap so you don't embed in blocks.
+        """
         dt = self.app.delta_time * 0.001
         if not self.on_ground:
             self.velocity.y -= GRAVITY * dt
@@ -34,8 +40,8 @@ class Player(Camera):
         new_position = glm.vec3(self.position)
         new_position.y += self.velocity.y
 
-        # partial snap approach
         if self.check_bounding_box_collision(new_position):
+            # partial snap
             sign = glm.sign(self.velocity.y)
             step_size = 0.02
             test_y = self.position.y
@@ -43,18 +49,16 @@ class Player(Camera):
             while True:
                 test_y += sign * step_size
                 test_pos = glm.vec3(self.position.x, test_y, self.position.z)
-
                 if self.check_bounding_box_collision(test_pos):
+                    # revert
                     test_y -= sign * step_size
                     break
-
-                # if we passed new_position.y, stop
+                # if we passed our proposed Y, stop
                 if (sign < 0 and test_y <= new_position.y) or (sign > 0 and test_y >= new_position.y):
                     break
 
             self.position.y = test_y
-            # landed if we were moving downward
-            if sign < 0:
+            if sign < 0:  # landed
                 self.on_ground = True
                 self.velocity.y = 0.0
         else:
@@ -70,15 +74,48 @@ class Player(Camera):
                 voxel_handler.switch_mode()
 
     def move(self, direction, velocity):
+        """
+        Attempts a horizontal move. If blocked by collision:
+          1) If on_ground, try to step up.
+          2) If still blocked, do nothing.
+        """
+        # 1) Build the new position for horizontal movement
         new_position = self.position + direction * velocity
-        if self.check_bounding_box_collision(new_position):
+        if not self.check_bounding_box_collision(new_position):
+            # no collision => accept move
+            self.position = new_position
+            return True
+
+        # 2) Collides. If not on_ground, can't step => blocked
+        if not self.on_ground:
             return False
-        self.position = new_position
+
+        # 3) Attempt a step-up
+        #    - First, raise current position by STEP_OFFSET
+        step_pos = glm.vec3(self.position)
+        step_pos.y += STEP_OFFSET  # try stepping up 1 block
+        if self.check_bounding_box_collision(step_pos):
+            return False  # can't even step up, blocked above
+
+        # 4) From the stepped position, move forward horizontally
+        step_pos += direction * velocity
+        if self.check_bounding_box_collision(step_pos):
+            return False  # blocked after stepping
+
+        # 5) Succeeded. Snap player to stepped position
+        self.position = step_pos
         return True
 
     def check_bounding_box_collision(self, test_pos):
+        """
+        Checks collisions for all 8 corners of the bounding box:
+          2*PLAYER_HALF_WIDTH in XZ,
+          2*HALF_HEIGHT in Y,
+        centered at 'test_pos'.
+        """
         voxel_handler = self.app.scene.world.voxel_handler
-        # bounding box corners around 'test_pos' as center
+
+        # 8 corners
         corners = [
             glm.vec3(-PLAYER_HALF_WIDTH, -HALF_HEIGHT, -PLAYER_HALF_WIDTH),
             glm.vec3( PLAYER_HALF_WIDTH, -HALF_HEIGHT, -PLAYER_HALF_WIDTH),
@@ -90,12 +127,14 @@ class Player(Camera):
             glm.vec3( PLAYER_HALF_WIDTH,  HALF_HEIGHT,  PLAYER_HALF_WIDTH),
             glm.vec3(-PLAYER_HALF_WIDTH,  HALF_HEIGHT,  PLAYER_HALF_WIDTH),
         ]
+
         for corner_offset in corners:
             corner_world_pos = test_pos + corner_offset
             if voxel_handler.is_colliding(corner_world_pos):
                 return True
         return False
 
+    # Movement helpers
     def move_left(self, velocity):
         self.move(-self.right, velocity)
 
@@ -118,6 +157,7 @@ class Player(Camera):
         if self.on_ground:
             self.velocity.y = JUMP_VELOCITY
             self.on_ground = False
+            # optional small upward nudge:
             self.move(self.up, velocity)
 
     def mouse_control(self):
