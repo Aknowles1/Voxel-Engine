bdf491c42fd51aa687b05fd99901bd9f2d3163f5
andrew.knowles <andrewkapranos@gmail.com>
2025-01-01 18:20:48 +1000

    Added random seed gen option

--- noise.py
+++ noise.py
@@ -1,15 +1,22 @@
 from settings import SEED
 from numba import njit
 from opensimplex.internals import _noise2, _noise3, _init
+import numpy as np
 
 perm, perm_grad_index3 = _init(seed=SEED)
 
+if SEED == 0:
+    rando = np.random.randint(0, 20000)
+    perm, perm_grad_index3 = _init(rando)
+else:
+    perm, perm_grad_index3 = _init(seed=SEED)
 
-@njit(cache=True)
+
+@njit(cache=False)
 def noise2(x, y):
     return _noise2(x, y, perm)
 
 
-@njit(cache=True)
+@njit(cache=False)
 def noise3(x, y, z):
     return _noise3(x, y, z, perm, perm_grad_index3)
